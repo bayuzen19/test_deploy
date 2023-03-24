86afc6478d303c22aeebddbfa9194091429b6011
Bayuzen Ahmad <88529383+bayuzen19@users.noreply.github.com>
2023-03-25 06:01:10 +0700

    Update machine_learning.py

--- pages/machine_learning.py
+++ pages/machine_learning.py
@@ -92,4 +92,4 @@ cols = ['fueltype', 'aspiration',
 if (st.button('Predict Car Price')):    
     sc, ohe, model = load('models/scaler.joblib', 'models/ohe.joblib', 'models/XGBoost.joblib')
     result = inference(row, cols, sc, ohe, model)
-    st.write(f"The predict price result is {price}")
+    st.write(f"The predict price result is {result}")
